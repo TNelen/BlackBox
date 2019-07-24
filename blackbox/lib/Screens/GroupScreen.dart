65256d1b9abe06a103c0006b087ca8057f95fef7
TNelen <timo.nelen@gmail.com>
2019-07-25 00:34:31 +0200

    impoved UI
    uniform top bar over all screens (with back arrow)
    
    adjusted the group screen top bar and bottom bar (only play button now)

--- blackbox/lib/Screens/GroupScreen.dart
+++ blackbox/lib/Screens/GroupScreen.dart
@@ -4,266 +4,251 @@ import '../Interfaces/Database.dart';
 import '../DataContainers/GroupData.dart';
 import 'WaitingScreen.dart';
 
-
-
-
-
-
-
-
-
 class GroupScreen extends StatelessWidget {
   GroupScreen(this.groupInfo) : super();
   final GroupData groupInfo;
 
   @override
   Widget build(BuildContext context) {
-
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
     return MaterialApp(
-      theme: new ThemeData(scaffoldBackgroundColor: Colors.black,
+      theme: new ThemeData(
+        scaffoldBackgroundColor: Colors.black,
       ),
       home: Scaffold(
-          body: DefaultTabController(
-              length: 3,
-              child: Scaffold(
-
-                appBar: AppBar(
-
-                  title: Container(
-                    padding: EdgeInsets.only( bottom: 5),
-                    child: Center(child: Text(groupInfo.groupName,
-                    style: TextStyle(
-                      fontSize: 35,
-                      color: Colors.white,
+        body: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Container(
+                padding: EdgeInsets.only(bottom: 5),
+                child: Row(
+                  children: [
+                    InkWell(
+                        onTap: () => Navigator.pop(context),
+                        child: Padding(
+                          padding: EdgeInsets.only(right: 20),
+                          child: const Icon(
+                            Icons.arrow_back_ios,
+                            color: Colors.amber,
+                          ),
+                        )),
+                    Center(
+                      child: Text(
+                        groupInfo.groupName,
+                        style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                  ),
-                  ),
-                  backgroundColor: Colors.black,
-                  bottom: TabBar(
-                    indicatorColor: Colors.amber,
-                    indicatorSize: TabBarIndicatorSize.label,
-
-                    tabs: [
-                      new Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          new Icon(
-                            Icons.group_add,
-                            color: Colors.white,
-                            //size: 25,
-
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 8),
-                            child: Text(
-                              'Invite',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
+                  ],
+                ),
+              ),
+              backgroundColor: Colors.black,
+              bottom: TabBar(
+                indicatorColor: Colors.amber,
+                indicatorSize: TabBarIndicatorSize.label,
+                tabs: [
+                  new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      new Icon(
+                        Icons.group_add,
+                        color: Colors.white,
+                        //size: 25,
                       ),
-                      new Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.people_outline,
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          'Invite',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
                             color: Colors.white,
-                            //size: 25,
-
                           ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 8),
-                            child: Text(
-                              'Members',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      new Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.settings,
-                            color: Colors.white,
-                            //size: 25,
-
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 8),
-                            child: Text(
-                              'Settings',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
                     ],
                   ),
-
-                ),
-                body: Stack(
-                  children: <Widget>[
-
-                TabBarView(children: [
-
-                  //tab 1
-                  Center(
-                  child: Container(
-                  alignment: Alignment(0, 0),
-                  child: RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      text: 'Display group code here + copy icon' ,
-                      style: TextStyle(color: Colors.white, fontSize: 50 ),
-
-
-                    ),),
-                   ),),
-
-                  //tab 2
-                  GridView.count(
-                  crossAxisCount: 2,
-                  padding: EdgeInsets.all(8.0),
-                  crossAxisSpacing: 12.0,
-                  mainAxisSpacing: 12.0,
-                  children: groupInfo.members
-                      .map((data) => Card(
-                    color: data == groupInfo.adminID  ? Colors.amber : Colors.white
-                    ,
-                    child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child:data == groupInfo.adminID  ?
-
-                          Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                            Text(data,
-                              style: new TextStyle(color: Colors.black, fontSize: 20.0,fontWeight: FontWeight.bold),),
-                            Text('Admin',
-                              style: new TextStyle(color: Colors.black, fontSize: 15.0,fontWeight: FontWeight.bold),),],),) :
-
-                          Text(data,
-                            style: new TextStyle(color: Colors.black, fontSize: 20.0,fontWeight: FontWeight.bold),)
-
-
-                              ,
-                        )),
-                  ))
-                      .toList(),
-                ),
-
-                //tab 3
-                  new Text('settings',
-                    style: new TextStyle(color: Colors.white, fontSize: 20.0),),
-
+                  new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.people_outline,
+                        color: Colors.white,
+                        //size: 25,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          'Members',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.settings,
+                        color: Colors.white,
+                        //size: 25,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          'Settings',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
-
-          ),
-            Align(
-              alignment: Alignment.bottomCenter,
-
-              child: _buildBottomCard(width, height, context)
+              ),
             ),
+            body: Stack(
+              children: <Widget>[
+                TabBarView(
+                  children: [
+                    //tab 1
+                    Center(
+                      child: Container(
+                        alignment: Alignment(0, 0),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            text: 'Display group code here + copy icon',
+                            style: TextStyle(color: Colors.white, fontSize: 50),
+                          ),
+                        ),
+                      ),
+                    ),
 
-              ],),),
+                    //tab 2
+                    GridView.count(
+                      crossAxisCount: 2,
+                      padding: EdgeInsets.all(8.0),
+                      crossAxisSpacing: 12.0,
+                      mainAxisSpacing: 12.0,
+                      children: groupInfo.members
+                          .map((data) => Card(
+                                color: data == groupInfo.adminID
+                                    ? Colors.amber
+                                    : Colors.white,
+                                child: Center(
+                                    child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: data == groupInfo.adminID
+                                      ? Center(
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: <Widget>[
+                                              Text(
+                                                data,
+                                                style: new TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 20.0,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              Text(
+                                                'Admin',
+                                                style: new TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 15.0,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ],
+                                          ),
+                                        )
+                                      : Text(
+                                          data,
+                                          style: new TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 20.0,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                )),
+                              ))
+                          .toList(),
+                    ),
+
+                    //tab 3
+                    new Text(
+                      'settings',
+                      style: new TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                  ],
+                ),
+                Align(
+                    alignment: Alignment.bottomCenter,
+                    child: _buildBottomCard(width, height, context)),
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
     );
   }
 
-  Widget _buildBottomCard(double width, double , BuildContext context) {
-
+  Widget _buildBottomCard(double width, double, BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
-    return
-      Container(
-        width: width,
-        height: height / 11,
-        padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
-        decoration: BoxDecoration(
-            color: Colors.amber ,
-            borderRadius: BorderRadius.only(
-                topRight: Radius.circular(32),
-                topLeft: Radius.circular(32)
-            )
-        ),
-        child: _buildBottomCardChildren(context),
-      );
+    return Container(
+      width: width,
+      height: height / 11,
+      padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+      decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(32), topLeft: Radius.circular(32))),
+      child: _buildBottomCardChildren(context),
+    );
   }
 
   Widget _buildBottomCardChildren(BuildContext context) {
     return Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                IconButton(onPressed: () {
-                  Navigator.pop(context);
-                }
-                  ,icon:Icon(Icons.list, size: 30,),padding: EdgeInsets.all(2),),
-                /*Text('Home',
-                    style: TextStyle(
-                        fontSize: 12,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),*/
-              ]
-          ),
-
-          Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                IconButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
                     builder: (BuildContext context) => WaitingScreen(groupInfo),
                   ));
-                }
-                  ,icon:Icon(Icons.play_arrow, size: 30,),padding: EdgeInsets.all(2),),
-                /*Text('Home',
+            },
+            icon: Icon(
+              Icons.play_arrow,
+              size: 50,
+            ),
+            padding: EdgeInsets.all(2),
+          ),
+          /*Text('Home',
                     style: TextStyle(
                         fontSize: 12,
                         color: Colors.black,
                         fontWeight: FontWeight.bold
                     ),
                   ),*/
-              ]
-          )
-
-        ]
-    );
+        ]);
   }
-
-
 }
-
-
-
-
-
