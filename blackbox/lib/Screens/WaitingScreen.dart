88873c5e33de0d0cc5ef1c78d2f5521e999678a6
TNelen <timo.nelen@gmail.com>
2019-07-25 23:50:07 +0200

    added results screen
    access this screen by pushing the readybutton in an admin screen

--- blackbox/lib/Screens/WaitingScreen.dart
+++ blackbox/lib/Screens/WaitingScreen.dart
@@ -2,98 +2,118 @@ import 'package:flutter/material.dart';
 import '../main.dart';
 import '../DataContainers/GroupData.dart';
 import '../Constants.dart';
-
-
+import 'ResultsScreen.dart';
 
 class WaitingScreen extends StatefulWidget {
-
   WaitingScreen(this.groupInfo) : super();
   final GroupData groupInfo;
 
   @override
-  _WaitingScreenState createState() => _WaitingScreenState( groupInfo );
-
+  _WaitingScreenState createState() => _WaitingScreenState(groupInfo);
 }
 
 class _WaitingScreenState extends State<WaitingScreen> {
-
   GroupData groupInfo;
 
   String username = Constants.username;
 
-  _WaitingScreenState(GroupData groupinfo)
-  {
+  _WaitingScreenState(GroupData groupinfo) {
     this.groupInfo = groupinfo;
   }
 
-
   @override
-
   Widget build(BuildContext context) {
-    if(Constants.username == groupInfo.adminID )
+    if (Constants.username == groupInfo.adminID)
       return adminScreen(context);
     else
       return userScreen(context);
   }
 }
 
-
-
-MaterialApp userScreen(BuildContext context){
+MaterialApp userScreen(BuildContext context) {
   return MaterialApp(
     theme: new ThemeData(scaffoldBackgroundColor: Colors.black),
     home: Scaffold(
-
       appBar: AppBar(
         title: IconButton(
-          icon: Icon(Icons.arrow_back_ios, color: Colors.amber,),
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.amber,
+          ),
           onPressed: () => Navigator.pop(context),
-
         ),
         backgroundColor: Colors.black,
       ),
-
       body: Center(
-
         child: Container(
           alignment: Alignment(0, 0),
           child: RichText(
             textAlign: TextAlign.center,
             text: TextSpan(
-              text: 'Waiting for people to join' ,
-              style: TextStyle(color: Colors.white, fontSize: 50 ),
-
-
-            ),),
-        ),),),);
+              text: 'Get Ready!'
+                  'waiting for admin to start game',
+              style: TextStyle(color: Colors.white, fontSize: 50),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
 
-MaterialApp adminScreen(BuildContext context){
+MaterialApp adminScreen(BuildContext context) {
+  final playButton = Material(
+    elevation: 5.0,
+    borderRadius: BorderRadius.circular(30.0),
+    color: Colors.amber,
+    child: MaterialButton(
+      minWidth: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      onPressed: () {Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) => ResultScreen(),
+        ));}, //change isplaying field in database for this group to TRUE
+      child: Text("Play",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20)
+              .copyWith(color: Colors.white, fontWeight: FontWeight.bold)),
+    ),
+  );
+
   return MaterialApp(
     theme: new ThemeData(scaffoldBackgroundColor: Colors.black),
     home: Scaffold(
-
       appBar: AppBar(
         title: IconButton(
-          icon: Icon(Icons.arrow_back_ios, color: Colors.amber,),
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.amber,
+          ),
           onPressed: () => Navigator.pop(context),
-
         ),
         backgroundColor: Colors.black,
       ),
-
       body: Center(
-
         child: Container(
+          padding: EdgeInsets.all(10),
           alignment: Alignment(0, 0),
-          child: RichText(
-            textAlign: TextAlign.center,
-            text: TextSpan(
-              text: 'You are admin' ,
-              style: TextStyle(color: Colors.white, fontSize: 50 ),
-
-
-            ),),
-        ),),
-      ),);
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: 'You are admin, press play if all members are ready',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+              SizedBox(height: 45.0),
+              playButton,
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
