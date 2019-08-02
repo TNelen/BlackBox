297cedcf7a9f976101e30d4340823202aa1ebb51
Dieter_ntm <nuytemans_dieter@hotmail.com>
2019-08-02 17:14:17 +0200

    ID <-> naam omgekeerd gefixt

--- blackbox/lib/Constants.dart
+++ blackbox/lib/Constants.dart
@@ -6,7 +6,7 @@ class Constants{
   static const List<String> choices = ['Create','Join'];
   static List<GroupData> groupData = [new GroupData('group1','Dit is de descriptionsshghmgh', 'id1', 'klootzak',['lid1','lid2','klootzak','lid2','lid2']),new GroupData('group2','Dit is de blablalala', 'id2', 'timo',['lid1','timo','lid2']),new GroupData('group3','I hope this text is showing with overflow on max 2 lines as it is supposed to do in case of an overflow', 'id3', 'lid2',['lid1','lid2']),new GroupData('group4','Elon = GOD', 'id4', 'timo',['lid1','lid2','timo','lid1','lid2']),new GroupData('group5','Falcon Heavy rocks', 'id5', 'lid8',['lid1','lid4','lid1','lid8','lid1','lid25578']),new GroupData('group5','Im a musk fanboy', 'id5', 'lid8',['lid1','lid4','lid1','lid8','lid1','lid25578']),new GroupData('group5','test for string overflow to see what the list tile layout does', 'id5', 'lid8',['lid1','lid4','lid1','lid8','lid1','lid25578']),new GroupData('group5','out of inspiration', 'id5', 'lid8',['lid1','lid4','lid1','lid8','lid1','lid25578']), new GroupData('groupX','Hello There!', 'id5', 'lid8',['lid1','lid4','lid1','lid8','lid1','lid25578','lid1','lid1','lid1','lid1','lid1','lid1'])];
 
-  static UserData userData = new UserData("timo", "someID");
+  static UserData userData = new UserData("GPY2pK6fqsdU0AU5IlGXhJpK8ej1", "Timo Nelen");
 
   /// Set the UserData of this user
   static void setUserData(UserData newData)
