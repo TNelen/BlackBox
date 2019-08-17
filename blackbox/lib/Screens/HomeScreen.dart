01843c04f26eb0713862e613ef329ce45985e75b
Dieter_ntm <nuytemans_dieter@hotmail.com>
2019-08-17 13:07:57 +0200

    Changed login check

--- blackbox/lib/Screens/HomeScreen.dart
+++ blackbox/lib/Screens/HomeScreen.dart
@@ -36,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     /// Log a user in and update variables accordingly
 
-    if (Constants.getUserID() == "GPY2pK6fqsdU0AU5IlGXhJpK8ej1" ) {
+    if (Constants.getUserID() == "GPY2pK6fqsdU0AU5IlGXhJpK8ej1" || Constants.getUserID() == "" ) {
       ///check if user isn't loged in via google already when returning to homescreen
       try {
         GoogleUserHandler guh = new GoogleUserHandler();
@@ -49,6 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
         print(e.toString());
       }
     }
+
 
     /* Example on how to add questions!
     /// Duplicates cannot be added so running it twice is not a problem
