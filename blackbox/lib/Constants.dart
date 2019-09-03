52aa10e416557b6671c6e4ee5afd3c89a8f26324
TNelen <timo.nelen@gmail.com>
2019-09-03 17:04:56 +0200

    changed userdata to standard

--- blackbox/lib/Constants.dart
+++ blackbox/lib/Constants.dart
@@ -48,7 +48,7 @@ class Constants{
 
   static const enable = [true, false];
 
-  static UserData userData = new UserData("2 ID", "gsm");
+  static UserData userData = new UserData("Some ID", "Elon");
   static final Database database = new Firebase();
 
   //setaccentColor
