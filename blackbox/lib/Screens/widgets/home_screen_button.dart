7eff4db1c7407b1510383b58101b39b014f048dc
TNelen <timo.nelen@gmail.com>
2020-11-26 21:25:51 +0100

    added new label to party mode

--- blackbox/lib/Screens/widgets/home_screen_button.dart
+++ blackbox/lib/Screens/widgets/home_screen_button.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:blackbox/Constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreenButton extends StatelessWidget {
@@ -17,50 +18,65 @@ class HomeScreenButton extends StatelessWidget {
       elevation: 5,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16.0),
-
       ),
       child: InkWell(
-        borderRadius: BorderRadius.circular(16.0),
-        splashColor: Constants.colors[Constants.colorindex],
-        onTap: () {
-          if (onTap != null) onTap();
-        },
-        child: Container(
-          padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
+          borderRadius: BorderRadius.circular(16.0),
+          splashColor: Constants.colors[Constants.colorindex],
+          onTap: () {
+            if (onTap != null) onTap();
+          },
+          child: Container(
+            padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
               Icon(
-                this.icon,
-                color: Constants.colors[Constants.colorindex],
-                size: 35,
+              this.icon,
+              color: Constants.colors[Constants.colorindex],
+              size: 35,
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+            Row(
+            //mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+              Text(
+              this._title,
+              style: TextStyle(fontSize: Constants.normalFontSize, color: Constants.iWhite, fontWeight: FontWeight.w400),
+            ),
+                this._title == "Party Mode" ? Card(
+              color: Constants.colors[Constants.colorindex],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
               ),
-              SizedBox(
-                width: 15,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    this._title,
-                    style: TextStyle(fontSize: Constants.normalFontSize, color: Constants.iWhite, fontWeight: FontWeight.w400),
-                  ),
-                  SizedBox(height: 5),
-                  Container(
-                    width: 230,
-                    child: AutoSizeText(
-                      this._description,
-                      style: TextStyle(fontSize: Constants.smallFontSize, color: Constants.iLight, fontWeight: FontWeight.w300),
-                      maxLines: 2,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+
+              child: Padding(
+                padding: EdgeInsets.all(3),
+                child: Text("New!",
+                  style: TextStyle(fontSize: Constants.smallFontSize, color: Constants.iBlack, fontWeight: FontWeight.w400),
+              ),),
+          ): SizedBox()
+          ]),
+      SizedBox(height: 5),
+      Container(
+        width: 230,
+        child: AutoSizeText(
+          this._description,
+          style: TextStyle(fontSize: Constants.smallFontSize, color: Constants.iLight, fontWeight: FontWeight.w300),
+          maxLines: 2,
         ),
       ),
+      ],
+    ),
+    ],
+    ),
+    ),
+    ),
     );
   }
 }
