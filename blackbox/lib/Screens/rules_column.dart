ada96522dd91b6bb6514bd02f8e4c3e3d7904bb6
TNelen <timo.nelen@gmail.com>
2021-03-07 20:02:41 +0100

    changed rules

--- blackbox/lib/Screens/rules_column.dart
+++ blackbox/lib/Screens/rules_column.dart
@@ -6,34 +6,24 @@ import 'package:blackbox/translations/rules_column.i18n.dart';
 class RulesColumn extends StatefulWidget {
   
   final List<String> titles = <String>[
-    'Party Mode'.i18n,
-    'Create Game'.i18n,
-    'Join Game'.i18n,
-    'Game settings'.i18n,
+    'Game'.i18n,
+    'Settings'.i18n,
+
   ];
 
   final List<String> descriptions = <String>[
-    // Party Mode
-    'Create a local game.'.i18n +
+    // Game
+    'You can select one or more categories when creating a game.'.i18n +
         '\n' +
         'All players vote one by one. After you have voted, pass the phone to the next player'.i18n +
         '\n' +
         'After everyone has voted, go to the results, and a new round starts.'.i18n,
 
-    // New game
-    'Create a new game and invite your friends by sharing the group code'.i18n,
-
-    // Join game
-    'Join a game with the 5 character group code'.i18n,
 
         // Game settings
     "The game creator can enable or disable 'blanco vote'. This is ability for the player to vote blanco".i18n +
-    '\n' +
-    "The game creator can enable or disable 'vote on self'. This controls whether the player is able to cast a vote on himself".i18n +
-    '\n' +
-    'These settings are set in the beginning of the game and cannot be changed during the game'.i18n
-
-
+    '\n'
+   
   ];
   
   @override
--- blackbox/lib/translations/rules_column.i18n.dart
+++ blackbox/lib/translations/rules_column.i18n.dart
@@ -3,24 +3,17 @@ import 'package:i18n_extension/i18n_extension.dart';
 extension Localization on String {
   static var _t = Translations("en") +
       {
-        "en": "Party Mode",
-        "nl": "Party Mode",
+        "en": "Settings",
+        "nl": "Instellingen",
+      } +
+    
+      {
+        "en": "Game ",
+        "nl": "Spel",
       } +
       {
-        "en": "Create Game",
-        "nl": "Maak een spel",
-      } +
-      {
-        "en": "Join Game",
-        "nl": "Meespelen",
-      } +
-      {
-        "en": "Game settings",
-        "nl": "Spel instellingen",
-      } +
-      {
-        "en": "Create a local game.",
-        "nl": "Maak een lokaal spel",
+        "en": "You can select one or more categories when creating a game.",
+        "nl": "Je kan een of meerdere categorieen selecteren wanneer je een spel maakt.",
       } +
       {
         "en":
@@ -34,35 +27,14 @@ extension Localization on String {
         "nl":
             "Als iedereen gestemd heeft, ga naar de resultaten en een nieuwe ronde kan gestart worden.",
       } +
-      {
-        "en":
-            "Create a new game and invite your friends by sharing the group code",
-        "nl":
-            "Maak een neiuw spel, en nodig je vrienden uit door de group code te delen",
-      } +
-      {
-        "en": "Join a game with the 5 character group code",
-        "nl":
-            "Speel mee met een spel door de groupcode van 5 karakters in te vullen",
-      } +
+     
       {
         "en":
             "The game creator can enable or disable 'blanco vote'. This is ability for the player to vote blanco",
         "nl":
             "De spel starter kan blanco voten aan of uit zetten. Dit zorgt ervoor dat je al dan niet een blanco stem kan uitbrengen.",
       } +
-      {
-        "en":
-            "The game creator can enable or disable 'vote on self'. This controls whether the player is able to cast a vote on himself",
-        "nl":
-            "De spel starter kan instellen of je al dan niet op jezelf kan stemmen",
-      } +
-      {
-        "en":
-            "These settings are set in the beginning of the game and cannot be changed during the game",
-        "nl":
-            "Deze instellingen worden ingesteld bij het begin van het spel en kunnen niet meer veranderd worden tijdens het spel.",
-      } +
+      
       {
         "en": "",
         "nl": "",
